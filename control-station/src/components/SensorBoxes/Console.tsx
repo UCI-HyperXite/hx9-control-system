c1e41c9c2cb96eb5557ce9e4764108e67c022677
Sam Der <sder@uci.edu>
2024-03-04 10:26:26 -0800

    Fix heading size

--- control-station/src/components/SensorBoxes/Console.tsx
+++ control-station/src/components/SensorBoxes/Console.tsx
@@ -3,7 +3,7 @@ import "./SensorData.css";
 function Console() {
 	return (
 		<div className="console">
-			<h1>Console</h1>
+			<h2>Console</h2>
 			<ul className="console-list">
 				<li className="console-list-item">Start Sent</li>
 				<li className="console-list-item">Stop Sent</li>
--- control-station/src/components/SensorBoxes/Sensors/SensorBox.tsx
+++ control-station/src/components/SensorBoxes/Sensors/SensorBox.tsx
@@ -4,7 +4,7 @@ function SensorBox() {
 	return (
 		<div className="sensorbox">
 			<h1 style={{ textAlign: "center", height: "10%" }}>Title</h1>
-			<h1 className="sensor-value">0</h1>
+			<h2 className="sensor-value">0</h2>
 		</div>
 	);
 }
