0f52eb1f366a4b43ff7bcc0ba3722b1c7ebc16aa
Sam Der <sder@uci.edu>
2024-03-04 11:06:18 -0800

    Rename status dir to Status

--- control-station/src/components/index.ts
+++ control-station/src/components/index.ts
@@ -1,4 +1,4 @@
 export { default as ControlPanel } from "./ControlPanel/ControlPanel";
 export { default as Navbar } from "./Navbar/Navbar";
 export { default as SensorData } from "./SensorBoxes/SensorData";
-export { default as Status } from "./status/Status";
+export { default as Status } from "./Status/Status";
