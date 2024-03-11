4282df42fb27a71b9b25980c53b959df57eee523
ryescholin <ryescholin@gmail.com>
2024-03-11 08:03:22 -0700

    test

--- pod-operation/src/handlers.rs
+++ pod-operation/src/handlers.rs
@@ -2,7 +2,19 @@ use socketioxide::extract::{AckSender, Data, SocketRef};
 use tracing::info;
 
 pub fn handle_ping(socket: SocketRef, Data(data): Data<String>, ack: AckSender) {
-	info!("Received event: {:?}", data);
-	ack.send("pong").ok();
-	socket.emit("pong", "pong").ok();
+    info!("Received event: {:?}", data);
+
+    let event = match data.as_str() {
+        "running" => Event::ClientRunning,
+        "stop" => Event::ClientStop,
+        "load" => Event::ClientLoad,
+        _ => {
+            error!("Unknown event received: {:?}", data);
+            return;
+        }
+    };
+    let state_machine = StateMachine { socket };
+    ack.send("pong").ok();
+    socket.emit("pong", "pong").ok();
+
 }
--- pod-operation/src/main.rs
+++ pod-operation/src/main.rs
@@ -3,27 +3,99 @@ use socketioxide::{extract::SocketRef, SocketIo};
 use tracing::{error, info};
 use tracing_subscriber::FmtSubscriber;
 
+use axum::Server;
+use socketioxide::SocketIo;
+use statig::{state_machine, Response::{Super, Transition}};
+use tracing::{error, info};
+use axum::CorsLayer;
+use tracing_subscriber::FmtSubscriber;
+use statig::Response;
+use socketioxide::extract::{AckSender, Data, SocketRef};
+
 mod handlers;
+
+#[derive(Default)]
+pub struct StateMachine {
+    socket: SocketRef,
+}
+pub enum Event {
+    Init,
+    ClientLoad,
+    ClientRunning,
+    ClientStop,
+}
+
+#[state_machine(initial = "State::init()")]
+impl StateMachine {
+    #[state]
+    fn load(&mut self, event: &Event) -> Response<State> {
+        // Emit event to client
+        self.socket.emit("load_event", "Load event occurred").ok();
+        
+        // State transitions
+        match event {
+            Event::ClientLoad => Transition(State::load()),
+            Event::ClientRunning => Transition(State::running()),
+            _ => Super,
+        }
+    }
+
+    #[state]
+    fn running(&mut self, event: &Event) -> Response<State> {
+        // Emit event to client
+        self.socket.emit("running_event", "Running event occurred").ok();
+
+        // State transitions
+        match event {
+            Event::ClientStop => Transition(State::stop()),
+            _ => Super,
+        }
+    }
+
+    #[state]
+    fn init(&mut self, event: &Event) -> Response<State> {
+        // Emit event to client
+        self.socket.emit("init_event", "Init event occurred").ok();
+
+        // State transitions
+        match event {
+            Event::ClientRunning => Transition(State::running()),
+            Event::ClientLoad => Transition(State::stop()),
+            _ => Super,
+        }
+    }
+
+    #[state]
+    fn stop(&mut self) -> Response<State> {
+        // Emit event to client
+        self.socket.emit("stop_event", "Stop event occurred").ok();
+
+        // State transition
+        Transition(State::init())
+    }
+}
+
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-	tracing::subscriber::set_global_default(FmtSubscriber::default())?;
+    tracing::subscriber::set_global_default(FmtSubscriber::default())?;
 
-	let (layer, io) = SocketIo::new_layer();
 
-	io.ns("/control-station", |socket: SocketRef| {
-		info!("Socket.IO connected: {:?} {:?}", socket.ns(), socket.id);
-		socket.on("ping", handlers::handle_ping);
-	});
+    let (layer, io) = SocketIo::new_layer();
 
-	let app = axum::Router::new().layer(layer);
+    io.ns("/control-station", |socket: SocketRef| {
+        info!("Socket.IO connected: {:?} {:?}", socket.ns(), socket.id);
+        socket.on("ping", handle_ping);
+    });
 
-	info!("Starting server on port 5000");
-	let server = Server::bind(&"127.0.0.1:5000".parse().unwrap()).serve(app.into_make_service());
+    let app = axum::Router::new().layer(layer).layer(CorsLayer::new());
 
-	if let Err(e) = server.await {
-		error!("server error: {}", e);
-	}
+    info!("Starting server on port 5000");
+    let server = Server::bind(&"0.0.0.0:5000".parse().unwrap()).serve(app.into_make_service());
 
-	Ok(())
+    if let Err(e) = server.await {
+        error!("server error: {}", e);
+    }
+
+    Ok(())
 }
