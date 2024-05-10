6d3c6158bf3f11d9d8001b6f949bd8b802f56761
ryescholin <46325761+ryescholin@users.noreply.github.com>
2024-05-10 16:25:05 -0700

    Provide preliminary state machine model (#39)
    - Create basic `StateMachine` which periodically emits sample data
      - Handle basic transition logic, to be improved later
      - An extra global state variable still needs to be cleaned up
    - Send socket messages from pod action buttons in control station
    
    ---------
    
    Co-authored-by: Sam Der <sder@uci.edu>

--- pod-operation/src/main.rs
+++ pod-operation/src/main.rs
@@ -1,14 +1,13 @@
+use crate::components::pressure_transducer::PressureTransducer;
 use axum::Server;
-use socketioxide::{extract::SocketRef, SocketIo};
+use socketioxide::SocketIo;
 use tracing::{error, info};
 use tracing_subscriber::FmtSubscriber;
-
 mod components;
 mod demo;
-mod handlers;
-
-use crate::components::pressure_transducer::PressureTransducer;
-use crate::components::signal_light::SignalLight;
+mod state_machine;
+use crate::state_machine::StateMachine;
+use std::{thread, time::Duration};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -16,20 +15,20 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 	let (layer, io) = SocketIo::new_layer();
 
-	io.ns("/control-station", |socket: SocketRef| {
-		info!("Socket.IO connected: {:?} {:?}", socket.ns(), socket.id);
-		socket.on("ping", handlers::handle_ping);
-	});
-
-	let signal_light = SignalLight::new();
-	tokio::spawn(demo::blink(signal_light));
+	thread::sleep(Duration::from_millis(1));
 
 	let pressure_transducer = PressureTransducer::new(0x40);
 	tokio::spawn(demo::read_pressure_transducer(pressure_transducer));
 
+	thread::spawn(move || {
+		let mut state_machine = StateMachine::new(io);
+		state_machine.run();
+	});
+
 	let app = axum::Router::new().layer(layer);
 
 	info!("Starting server on port 5000");
+
 	let server = Server::bind(&"127.0.0.1:5000".parse().unwrap()).serve(app.into_make_service());
 
 	if let Err(e) = server.await {
