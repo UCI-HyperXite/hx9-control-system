7061483ec98ea5c46612d0f941c049ce8bb61d6d
ryescholin <ryescholin@gmail.com>
2024-05-14 22:49:25 -0700

    cln

--- pod-operation/src/components/wheel_encoder.rs
+++ pod-operation/src/components/wheel_encoder.rs
@@ -36,8 +36,8 @@ impl WheelEncoder {
 		let a_state = self.pin_a.read();
 		let b_state = self.pin_b.read();
 		if (a_state != self.a_last_read || b_state != self.b_last_read) && b_state != a_state {
-            self.counter += 1.0;
-        }
+			self.counter += 1.0;
+		}
 		self.a_last_read = a_state;
 		self.b_last_read = b_state;
 
