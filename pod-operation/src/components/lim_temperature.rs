f3f68882784729f7231af73d143731156096ce0d
Sam Der <sder@uci.edu>
2024-05-16 12:30:05 -0700

    Update comments for thermistor constants

--- pod-operation/src/components/lim_temperature.rs
+++ pod-operation/src/components/lim_temperature.rs
@@ -10,11 +10,11 @@ use rppal::i2c::I2c;
 // connected to a thermistor, with the ADS1015 is measuring the node connecting
 // the two (a voltage divider circuit).
 
-const DIVIDER_RESISTANCE: f32 = 22000.0; // Resistance used in voltage divider circuit
-const V_IN: f32 = 5.0; // Voltage provided to voltage divider circuit
-const BETA: f32 = 3950.0; // Constant used in voltage to temperature conversion process
-const R_0: f32 = 10000.0; // Resistance of thermistor at room temperature
-const ROOM_TEMP: f32 = 298.15; // Room temperature in Kelvins (25 degrees C)
+const DIVIDER_RESISTANCE: f32 = 22000.0; // Ohms
+const V_IN: f32 = 5.0; // Volts
+const BETA: f32 = 3950.0; // Kelvins
+const R_0: f32 = 10000.0; // Ohms
+const ROOM_TEMP: f32 = 298.15; // Kelvins
 
 fn voltage_to_temp(voltage: i16) -> f32 {
 	let voltage = f32::from(voltage) / 1000.0;
