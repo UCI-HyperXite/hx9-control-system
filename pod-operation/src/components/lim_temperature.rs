32a49965849958363f3e0e7012a66c7bb8224bc6
Sam Der <sder@uci.edu>
2024-05-16 11:09:31 -0700

    Add voltage to temperature conversion function

--- pod-operation/src/components/lim_temperature.rs
+++ pod-operation/src/components/lim_temperature.rs
@@ -6,6 +6,19 @@ use ads1x1x::{Ads1x1x, DynamicOneShot, SlaveAddr};
 use nb::block;
 use rppal::i2c::I2c;
 
+const DIVIDER_RESISTANCE: f32 = 22000.0;
+const VCC: f32 = 5.0;
+const BETA: f32 = 3950.0;
+const R_0: f32 = 10000.0;
+const ROOM_TEMP: f32 = 298.15;
+
+fn voltage_to_temp(voltage: i16) -> f32 {
+	let voltage = f32::from(voltage) / 1000.0;
+	let thermistor_resistance: f32 = (voltage * DIVIDER_RESISTANCE) / (VCC - voltage);
+	let r_inf = R_0 * std::f32::consts::E.powf(-BETA / ROOM_TEMP);
+	(BETA / (thermistor_resistance / r_inf).ln()) - 273.15
+}
+
 pub struct LimTemperature {
 	ads1015: Ads1x1x<I2cInterface<I2c>, Ads1015, Resolution12Bit, OneShot>,
 }
@@ -21,9 +34,10 @@ impl LimTemperature {
 		self.ads1015.destroy_ads1015();
 	}
 
-	pub fn read_pins(&mut self) -> (i16, i16, i16, i16) {
+	pub fn read_pins(&mut self) -> (f32, f32, f32, f32) {
 		[SingleA0, SingleA1, SingleA2, SingleA3]
 			.map(|channel| block!(self.ads1015.read(channel)).unwrap())
+			.map(voltage_to_temp)
 			.into()
 	}
 }
