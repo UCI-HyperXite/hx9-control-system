da8c354c2cd8cce2d72bdf5d1c7895f8bee1aef4
ryescholin <ryescholin@gmail.com>
2024-05-26 13:27:40 -0700

    chk

--- pod-operation/src/components/mod.rs
+++ pod-operation/src/components/mod.rs
@@ -2,7 +2,7 @@
 // pub mod gyro;
 // pub mod high_voltage_system;
 // pub mod lim_current;
-pub mod lim_temperature;
-// pub mod pressure_transducer;
+//pub mod lim_temperature;
+ pub mod pressure_transducer;
 // pub mod signal_light;
 // pub mod wheel_encoder;
