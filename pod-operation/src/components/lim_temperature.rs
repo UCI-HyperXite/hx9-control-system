96c11c3d31c5dc05fb7aa4794c55df65e19bced2
ryescholin <ryescholin@gmail.com>
2024-05-25 19:39:12 -0700

    imp

--- pod-operation/src/components/lim_temperature.rs
+++ pod-operation/src/components/lim_temperature.rs
@@ -2,6 +2,7 @@ use ads1x1x::ic::{Ads1015, Resolution12Bit};
 use ads1x1x::interface::I2cInterface;
 use ads1x1x::mode::OneShot;
 use ads1x1x::ChannelSelection::{SingleA0, SingleA1, SingleA2, SingleA3};
+use ads1x1x::FullScaleRange;
 use ads1x1x::{Ads1x1x, DynamicOneShot, SlaveAddr};
 use nb::block;
 use rppal::i2c::I2c;
@@ -34,8 +35,8 @@ pub struct LimTemperature {
 impl LimTemperature {
 	pub fn new(device_address: SlaveAddr) -> Self {
 		let i2cdev = I2c::new().unwrap();
-		let adc = Ads1x1x::new_ads1015(i2cdev, device_address);
-		adc.set_full_scale_range(FullScaleRange::Within4_096V)
+		let adc = Ads1x1x::new_ads1015(i2cdev, device_address)
+			.set_full_scale_range(FullScaleRange::Within4_096V)
 			.unwrap();
 		LimTemperature { ads1015: adc }
 	}
