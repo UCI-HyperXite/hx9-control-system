f0ddaeb93d50aca6cda525df3531822a881ad9d6
ryescholin <ryescholin@gmail.com>
2024-05-28 20:14:04 -0700

    clean up

--- pod-operation/src/components/lidar.rs
+++ pod-operation/src/components/lidar.rs
@@ -1,27 +1,26 @@
-use lidar_lite_v3::{LidarLiteV3, Status};
 use i2cdev::linux::LinuxI2CDevice;
+use lidar_lite_v3::{LidarLiteV3, Status};
 use std::error::Error;
 
 pub struct Lidar {
-    lidar_lite: LidarLiteV3<LinuxI2CDevice>,
+	lidar_lite: LidarLiteV3<LinuxI2CDevice>,
 }
 
 impl Lidar {
-    pub fn new() -> Lidar {
-        // Initialize the I2C device
-        let i2cdev = LinuxI2CDevice::new("/dev/i2c-1", 0x62)
-            .expect("Failed to initialize I2C device");
-        // Create LidarLiteV3 instance
-        let mut lidar_lite = LidarLiteV3::new(i2cdev)
-            .expect("Failed to initialize LidarLiteV3");
-       
-        Lidar { lidar_lite }
-    }
+	pub fn new() -> Lidar {
+		// Initialize the I2C device
+		let i2cdev =
+			LinuxI2CDevice::new("/dev/i2c-1", 0x62).expect("Failed to initialize I2C device");
+		// Create LidarLiteV3 instance
+		let mut lidar_lite = LidarLiteV3::new(i2cdev).expect("Failed to initialize LidarLiteV3");
 
-    pub fn read_distance(&mut self) -> f32 {
-        let distance = self.lidar_lite.read_distance(false).unwrap();
-        let distance_meters = distance as f32 / 100.0;
+		Lidar { lidar_lite }
+	}
 
-        distance_meters
-    }
+	pub fn read_distance(&mut self) -> f32 {
+		let distance = self.lidar_lite.read_distance(false).unwrap();
+		let distance_meters = distance as f32 / 100.0;
+
+		distance_meters
+	}
 }
--- pod-operation/src/main.rs
+++ pod-operation/src/main.rs
@@ -5,17 +5,17 @@ use tracing_subscriber::FmtSubscriber;
 
 mod components;
 mod demo;
-// mod state_machine;
+mod state_machine;
 
-// use crate::components::brakes::Brakes;
-// use crate::components::gyro::Gyroscope;
-// use crate::components::high_voltage_system::HighVoltageSystem;
-// use crate::components::lim_temperature::LimTemperature;
-// use crate::components::pressure_transducer::PressureTransducer;
-// use crate::components::signal_light::SignalLight;
-// use crate::components::wheel_encoder::WheelEncoder;
+use crate::components::brakes::Brakes;
+use crate::components::gyro::Gyroscope;
+use crate::components::high_voltage_system::HighVoltageSystem;
 use crate::components::lidar::Lidar;
-// use crate::state_machine::StateMachine;
+use crate::components::lim_temperature::LimTemperature;
+use crate::components::pressure_transducer::PressureTransducer;
+use crate::components::signal_light::SignalLight;
+use crate::components::wheel_encoder::WheelEncoder;
+use crate::state_machine::StateMachine;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -23,40 +23,38 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 	let (layer, io) = SocketIo::new_layer();
 
-	// let signal_light = SignalLight::new();
-	// tokio::spawn(demo::blink(signal_light));
+	let signal_light = SignalLight::new();
+	tokio::spawn(demo::blink(signal_light));
 
-	// let upstream_pressure_transducer = PressureTransducer::upstream();
-	// tokio::spawn(demo::read_pressure_transducer(upstream_pressure_transducer));
+	let upstream_pressure_transducer = PressureTransducer::upstream();
+	tokio::spawn(demo::read_pressure_transducer(upstream_pressure_transducer));
 
-	// let downstream_pressure_transducer = PressureTransducer::downstream();
-	// tokio::spawn(demo::read_pressure_transducer(
-	// 	downstream_pressure_transducer,
-	// ));
+	let downstream_pressure_transducer = PressureTransducer::downstream();
+	tokio::spawn(demo::read_pressure_transducer(
+		downstream_pressure_transducer,
+	));
 
-	// let ads1015 = LimTemperature::new(ads1x1x::SlaveAddr::Default);
-	// tokio::spawn(demo::read_ads1015(ads1015));
+	let ads1015 = LimTemperature::new(ads1x1x::SlaveAddr::Default);
+	tokio::spawn(demo::read_ads1015(ads1015));
 
-	// let wheel_encoder = WheelEncoder::new();
-	// tokio::spawn(demo::read_wheel_encoder(wheel_encoder));
+	let wheel_encoder = WheelEncoder::new();
+	tokio::spawn(demo::read_wheel_encoder(wheel_encoder));
 
-	// let gyro: Gyroscope = Gyroscope::new();
-	// tokio::spawn(demo::read_gyroscope(gyro));
-	// let brakes = Brakes::new();
-	// tokio::spawn(demo::brake(brakes));
+	let gyro: Gyroscope = Gyroscope::new();
+	tokio::spawn(demo::read_gyroscope(gyro));
+	let brakes = Brakes::new();
+	tokio::spawn(demo::brake(brakes));
 
-	// let high_voltage_system = HighVoltageSystem::new();
-	// tokio::spawn(demo::high_voltage_system(high_voltage_system));
+	let high_voltage_system = HighVoltageSystem::new();
+	tokio::spawn(demo::high_voltage_system(high_voltage_system));
 
 	let lidar = Lidar::new();
 	tokio::spawn(demo::read_lidar(lidar));
 
-
-	
-	// tokio::spawn(async {
-	// 	let mut state_machine = StateMachine::new(io);
-	// 	state_machine.run().await;
-	// });
+	tokio::spawn(async {
+		let mut state_machine = StateMachine::new(io);
+		state_machine.run().await;
+	});
 
 	let app = axum::Router::new().layer(layer);
 
