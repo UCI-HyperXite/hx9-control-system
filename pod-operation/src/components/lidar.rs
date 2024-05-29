747dc01de8eab5607ee9c3164de56a4f661b06e1
ryescholin <ryescholin@gmail.com>
2024-05-29 15:40:29 -0700

    notes

--- pod-operation/src/components/lidar.rs
+++ pod-operation/src/components/lidar.rs
@@ -1,16 +1,16 @@
 use i2cdev::linux::LinuxI2CDevice;
 use lidar_lite_v3::LidarLiteV3;
 
+const I2C_ADDRESS: str = "/dev/i2c-1";
+
 pub struct Lidar {
 	lidar_lite: LidarLiteV3<LinuxI2CDevice>,
 }
 
 impl Lidar {
 	pub fn new() -> Lidar {
-		// Initialize the I2C device
 		let i2cdev =
-			LinuxI2CDevice::new("/dev/i2c-1", 0x62).expect("Failed to initialize I2C device");
-		// Create LidarLiteV3 instance
+			LinuxI2CDevice::new(I2C_ADDRESS, 0x62).expect("Failed to initialize I2C device");
 		let lidar_lite = LidarLiteV3::new(i2cdev).expect("Failed to initialize LidarLiteV3");
 
 		Lidar { lidar_lite }
@@ -18,6 +18,8 @@ impl Lidar {
 
 	pub fn read_distance(&mut self) -> f32 {
 		let distance = self.lidar_lite.read_distance(false).unwrap();
-		distance as f32 / 100.0
+		/// Convert the distance to meters
+		f32::from(distance)
+			/ 100.0
 	}
 }
