030f4f95b73988795ce7aa6f5fac52dc66b00923
ryescholin <ryescholin@gmail.com>
2024-05-29 15:45:14 -0700

    whoops

--- pod-operation/src/components/lidar.rs
+++ pod-operation/src/components/lidar.rs
@@ -1,7 +1,7 @@
 use i2cdev::linux::LinuxI2CDevice;
 use lidar_lite_v3::LidarLiteV3;
 
-const I2C_ADDRESS: &str = "/dev/i2c-1";
+const LIDAR_ADDRESS: u16 = 0x62;
 
 pub struct Lidar {
 	lidar_lite: LidarLiteV3<LinuxI2CDevice>,
@@ -9,8 +9,8 @@ pub struct Lidar {
 
 impl Lidar {
 	pub fn new() -> Lidar {
-		let i2cdev =
-			LinuxI2CDevice::new(I2C_ADDRESS, 0x62).expect("Failed to initialize I2C device");
+		let i2cdev = LinuxI2CDevice::new("/dev/i2c-1", LIDAR_ADDRESS)
+			.expect("Failed to initialize I2C device");
 		let lidar_lite = LidarLiteV3::new(i2cdev).expect("Failed to initialize LidarLiteV3");
 
 		Lidar { lidar_lite }
