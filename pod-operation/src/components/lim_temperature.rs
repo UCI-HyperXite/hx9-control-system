4111dd35333873cc96117a32bd81322ed200e7fd
ryescholin <ryescholin@gmail.com>
2024-05-31 00:15:58 -0700

    remove full scale, etc

--- pod-operation/src/components/lim_temperature.rs
+++ pod-operation/src/components/lim_temperature.rs
@@ -2,7 +2,6 @@ use ads1x1x::ic::{Ads1015, Resolution12Bit};
 use ads1x1x::interface::I2cInterface;
 use ads1x1x::mode::OneShot;
 use ads1x1x::ChannelSelection::{SingleA0, SingleA1, SingleA2, SingleA3};
-use ads1x1x::FullScaleRange;
 use ads1x1x::{Ads1x1x, DynamicOneShot, SlaveAddr};
 use nb::block;
 use rppal::i2c::I2c;
@@ -33,9 +32,7 @@ pub struct LimTemperature {
 impl LimTemperature {
 	pub fn new(device_address: SlaveAddr) -> Self {
 		let i2cdev = I2c::new().unwrap();
-		let mut adc = Ads1x1x::new_ads1015(i2cdev, device_address);
-		adc.set_full_scale_range(FullScaleRange::Within4_096V)
-			.unwrap();
+		let adc = Ads1x1x::new_ads1015(i2cdev, device_address);
 		LimTemperature { ads1015: adc }
 	}
 
@@ -45,7 +42,7 @@ impl LimTemperature {
 
 	pub fn read_lim_temps(&mut self) -> (f32, f32, f32, f32) {
 		[SingleA0, SingleA1, SingleA2, SingleA3]
-			.map(|channel| f32::from(block!(self.ads1015.read(channel)).unwrap()) / 500.0)
+			.map(|channel| f32::from(block!(self.ads1015.read(channel)).unwrap()) / 1000.0)
 			.map(voltage_to_temp)
 			.into()
 	}
