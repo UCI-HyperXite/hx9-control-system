7987c5b4f26422e94bf9fba624db14ba33d3b999
vrushang1234 <vrushanganand20@gmail.com>
2024-05-31 16:05:26 -0700

    Added state transition messages

--- control-station/src/components/SensorBoxes/Console.tsx
+++ control-station/src/components/SensorBoxes/Console.tsx
@@ -1,10 +1,26 @@
+import { useContext, useEffect, useState } from "react";
 import "./SensorData.css";
+import PodContext from "@/services/PodContext";
 
 function Console() {
+	const { podData } = useContext(PodContext);
+	const [stateList, setStateList] = useState<string[]>([]);
+
+	useEffect(() => {
+		if (podData.state) {
+			setStateList((prev) => [...prev, podData.state]);
+		}
+	}, [podData.state]);
+
 	return (
 		<div className="console">
 			<h2>Console</h2>
 			<ul className="console-list">
+				{stateList.map((prop, index) => (
+					<li key={index} className="console-list-item">
+						{prop} State
+					</li>
+				))}
 				<li className="console-list-item">Start Sent</li>
 				<li className="console-list-item">Stop Sent</li>
 				<li className="console-list-item">Load Sent</li>
