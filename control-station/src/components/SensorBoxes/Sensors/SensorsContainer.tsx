beab218d2e3435156d7fc9226d00cfdbdac098c5
vrushang1234 <vrushanganand20@gmail.com>
2024-05-31 20:30:09 -0700

    Made serverResponse Partial

--- control-station/src/components/SensorBoxes/Sensors/SensorsContainer.tsx
+++ control-station/src/components/SensorBoxes/Sensors/SensorsContainer.tsx
@@ -4,13 +4,19 @@ import PodContext from "@/services/PodContext";
 import StatusIndicator from "@/components/StatusIndicator/StatusIndicator";
 function SensorContainer() {
 	const { podData } = useContext(PodContext);
+	const {
+		wheel_encoder,
+		downstream_pressure_transducer,
+		upstream_pressure_transducer,
+		lim_temperature_port,
+	} = podData;
 	return (
 		<div className="SensorContainer">
-			<SensorBox title="Speed" value={podData.wheel_encoder.velocity} />
-			<SensorBox title="Distance" value={podData.wheel_encoder.distance} />
-			<SensorBox title="PT1" value={podData.downstream_pressure_transducer} />
-			<SensorBox title="PT2" value={podData.upstream_pressure_transducer} />
-			<SensorBox title="Lim Current" value={podData.upstream_pressure_transducer} />
+			<SensorBox title="Speed" value={wheel_encoder.velocity} />
+			<SensorBox title="Distance" value={wheel_encoder.distance} />
+			<SensorBox title="PT1" value={downstream_pressure_transducer} />
+			<SensorBox title="PT2" value={upstream_pressure_transducer} />
+			<SensorBox title="Lim Current" value={lim_temperature_port} />
 			<StatusIndicator />
 		</div>
 	);
