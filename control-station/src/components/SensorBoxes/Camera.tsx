9a66976519149c37aefb7b3e76209cceed6fb81e
vrushang1234 <vrushanganand20@gmail.com>
2024-05-31 20:07:44 -0700

    Initial commit

--- control-station/src/components/SensorBoxes/Camera.tsx
+++ control-station/src/components/SensorBoxes/Camera.tsx
@@ -1,7 +1,19 @@
 import "./SensorData.css";
-
+import { LineChart } from "@mui/x-charts";
 function Camera() {
-	return <div className="camera"></div>;
+	return (
+		<div className="camera">
+			<LineChart
+				xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+				series={[
+					{
+						data: [2, 5.5, 2, 8.5, 1.5, 5],
+					},
+				]}
+				className="lidar-chart"
+			/>
+		</div>
+	);
 }
 
 export default Camera;
