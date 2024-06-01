f6bebc107f109424d58b79775607bd830f2db9a9
vrushang1234 <vrushanganand20@gmail.com>
2024-05-31 23:40:24 -0700

    Added dynamics page and react router

--- control-station/src/App.tsx
+++ control-station/src/App.tsx
@@ -1,7 +1,9 @@
 import { ControlPanel, Navbar } from "@/components";
 import usePodData from "./services/usePodData";
 import PodContext from "./services/PodContext";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Dashboard } from "@/views";
+import Dynamics from "./components/Dynamics/Dynamics";
 
 function App() {
 	const { podData, podSocketClient } = usePodData();
@@ -10,7 +12,12 @@ function App() {
 		<main>
 			<PodContext.Provider value={{ podData, podSocketClient }}>
 				<Navbar />
-				<Dashboard />
+				<BrowserRouter>
+					<Routes>
+						<Route path="/" element={<Dashboard />} />
+						<Route path="/dynamics" element={<Dynamics />} />
+					</Routes>
+				</BrowserRouter>
 				<ControlPanel />
 			</PodContext.Provider>
 		</main>
