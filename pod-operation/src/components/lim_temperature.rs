e9e6dd768d40277bfc93feef1eecc5c66a0c04f9
brigitte <brigitte@yi-ann.lan>
2024-10-18 06:48:36 -0700

    refactored rppal references to not use it in mock implementation

--- pod-operation/src/components/lim_temperature.rs
+++ pod-operation/src/components/lim_temperature.rs
@@ -1,4 +1,4 @@
-// wrote in a separate module for ads1015 
+// Define a separate module for ads1015-related functionality
 #[cfg(feature = "ads1015")]
 pub mod ads1015_plswork {
     use ads1x1x::ic::{Ads1015, Resolution12Bit};
@@ -42,7 +42,7 @@ pub mod ads1015_plswork {
         }
 
         pub fn cleanup(self) {
-            // no specific cleanup reqs for ads1015
+            // No specific cleanup required for ADS1015
         }
 
         pub fn read_lim_temps(&mut self) -> [f32; 4] {
@@ -53,7 +53,7 @@ pub mod ads1015_plswork {
     }
 }
 
-// redefined main module :D 
+// Main module
 use ads1x1x::SlaveAddr;
 use tracing::info;
 
@@ -71,25 +71,22 @@ impl LimTemperature {
     }
 
     pub fn cleanup(self) {
-        
+        // No cleanup required for the mock
     }
 
     pub fn read_lim_temps(&mut self) -> [f32; 4] {
-        [0.45, 0.45, 0.45, 0.45].map(voltage_to_temp)
+        [0.45, 0.45, 0.45, 0.45].map(|voltage| {
+            const C_TO_K_CONVERSION: f32 = 273.15;
+            const DIVIDER_RESISTANCE: f32 = 1000.0; // Ohms
+            const V_IN: f32 = 5.0; // Volts
+            const BETA: f32 = 3950.0; // Kelvins
+            const R_0: f32 = 10000.0; // Ohms
+            const ROOM_TEMP: f32 = 25.0 + C_TO_K_CONVERSION; // Kelvins
+
+            let thermistor_resistance = ((V_IN - voltage) * DIVIDER_RESISTANCE) / voltage;
+            let r_inf = R_0 * std::f32::consts::E.powf(-BETA / ROOM_TEMP);
+            let temp_kelvins = BETA / (thermistor_resistance / r_inf).ln();
+            temp_kelvins - C_TO_K_CONVERSION
+        })
     }
-}
-
-#[cfg(not(feature = "ads1015"))]
-fn voltage_to_temp(voltage: f32) -> f32 {
-    const C_TO_K_CONVERSION: f32 = 273.15;
-    const DIVIDER_RESISTANCE: f32 = 1000.0; // Ohms
-    const V_IN: f32 = 5.0; // Volts
-    const BETA: f32 = 3950.0; // Kelvins
-    const R_0: f32 = 10000.0; // Ohms
-    const ROOM_TEMP: f32 = 25.0 + C_TO_K_CONVERSION; // Kelvins
-
-    let thermistor_resistance = ((V_IN - voltage) * DIVIDER_RESISTANCE) / voltage;
-    let r_inf = R_0 * std::f32::consts::E.powf(-BETA / ROOM_TEMP);
-    let temp_kelvins = BETA / (thermistor_resistance / r_inf).ln();
-    temp_kelvins - C_TO_K_CONVERSION
 }
